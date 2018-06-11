b10c6ea0bff13f94f96020ebfe609678eb92a08e
Ivan Franchin <ivan.franchin@takeaway.com>
2018-06-11 17:10:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# docker-swarm-environment
+
+## Goal
+
+The goal of this project is have some examples using [`Docker Swarm`](https://docs.docker.com/engine/swarm/swarm-tutorial)
+
+### `keycloak-clustered`
+
+The goal of this project is to deploy [`keycloak-clustered`](https://github.com/ivangfr/keycloak-clustered) instances in `Docker Swarm`.
+
+### More soon
---
+++ keycloak-clustered/setup-docker-swarm.sh
@@ -0,0 +1,29 @@
+echo 
+echo "Creating machines"
+echo "-----------------"
+docker-machine create --driver virtualbox manager1
+docker-machine create --driver virtualbox manager2
+docker-machine create --driver virtualbox worker1
+docker-machine create --driver virtualbox worker2
+
+echo 
+echo "Starting swarm"
+echo "--------------"
+MANAGER1_IP=$(docker-machine ip manager1)
+
+docker-machine ssh manager1 docker swarm init --advertise-addr $MANAGER1_IP
+
+echo 
+echo "Adding members"
+echo "--------------"
+MANAGER_TOKEN=$(docker-machine ssh manager1 docker swarm join-token --quiet manager)
+WORKER_TOKEN=$(docker-machine ssh manager1 docker swarm join-token --quiet worker)
+
+docker-machine ssh manager2 docker swarm join --token $MANAGER_TOKEN $MANAGER1_IP:2377
+docker-machine ssh worker1 docker swarm join --token $WORKER_TOKEN $MANAGER1_IP:2377
+docker-machine ssh worker2 docker swarm join --token $WORKER_TOKEN $MANAGER1_IP:2377
+
+echo 
+echo "Creating overlay network"
+echo "------------------------"
+docker-machine ssh manager1 docker network create --driver overlay my-swarm-net
